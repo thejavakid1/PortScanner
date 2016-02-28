75b05d8de728ca71c953a56a9bc9bf7dc569cdb8
thejavakid1 <thejavakid1@gmail.com>
2016-02-27 19:29:00 -0800

    added crash options

--- portscanner/PortScanner.java
+++ portscanner/PortScanner.java
@@ -5,7 +5,7 @@
 package portscanner;
 
 import portscanner.Gui.DoneGui;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author thejavakid
@@ -27,6 +27,23 @@ public class PortScanner {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(DoneGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+	boolean ReRun = true;
+	while (ReRun = true){
+	    try {
+		startGUI();
+	    }catch(Exception e){
+		    int reply = JOptionPane.showConfirmDialog(null, "Sorry, PortScanner has crashed. Do you wish to restart PortScanner?", "PortScanner has crashed.", JOptionPane.YES_NO_OPTION);
+		    if (reply == JOptionPane.YES_OPTION) {
+			ReRun = true;
+		    }
+		    else {
+			System.exit(0);
+		    }
+	    }
+	}
+    }
+
+    public static void startGUI(){
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new DoneGui().setVisible(true);
